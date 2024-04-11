c2b6d0ec80d9f04789368239e762387897b5bff5
roshan <mailme.roshanbhattarai@gmail.com>
2024-04-11 11:10:51 -0500

    remove storage files

--- src/business/LibraryMember.java
+++ src/business/LibraryMember.java
@@ -1,32 +1,34 @@
 package business;
 
 import java.io.Serializable;
-import java.time.LocalDate;
 
-
-import dataaccess.DataAccess;
-import dataaccess.DataAccessFacade;
 
 final public class LibraryMember extends Person implements Serializable {
+	private static final long serialVersionUID = -2226197306790714013L;
 	private String memberId;
-	
+	private CheckoutRecord checkoutRecord;
+
 	public LibraryMember(String memberId, String fname, String lname, String tel,Address add) {
 		super(fname,lname, tel, add);
-		this.memberId = memberId;		
+		this.memberId = memberId;
 	}
-	
-	
+
 	public String getMemberId() {
 		return memberId;
 	}
 
-	
-	
-	@Override
-	public String toString() {
-		return "Member Info: " + "ID: " + memberId + ", name: " + getFirstName() + " " + getLastName() + 
-				", " + getTelephone() + " " + getAddress();
+	public void addCheckoutRecord(Book book){
+		if(this.checkoutRecord == null){
+			this.checkoutRecord = new CheckoutRecord(this, book);
+		}else{
+			this.checkoutRecord.addRecord(book);
+		}
 	}
 
-	private static final long serialVersionUID = -2226197306790714013L;
+	@Override
+	public String toString() {
+		return "Member: " + "ID: " + memberId + ", name: " + getFirstName() + " " + getLastName() +
+				", phone: " + getTelephone() + ", address: " + getAddress();
+	}
+
 }
