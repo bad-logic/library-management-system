6c883feb6c9bf5ad56b71c62de40d40a8ae00be7
roshan <45297269+bad-logic@users.noreply.github.com>
2024-04-11 13:33:29 -0500

    update add authors

--- src/business/Author.java
+++ src/business/Author.java
@@ -1,19 +1,26 @@
 package business;
 
 import java.io.Serializable;
+import java.util.UUID;
 
 final public class Author extends Person implements Serializable {
 	private String bio;
+	private String authorId;
 	public String getBio() {
 		return this.bio;
 	}
 	
 	public Author(String f, String l, String t, Address a, String bio) {
 		super(f, l, t, a);
+		this.authorId = UUID.randomUUID().toString();
 		this.bio = bio;
 	}
 
 	private static final long serialVersionUID = 7508481940058530471L;
+
+	public String getAuthorId() {
+		return this.authorId;
+	}
 
 	@Override
 	public boolean equals(Object obj) {
