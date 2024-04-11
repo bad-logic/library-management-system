cc883696995449af22a3b6d0929ef25c44a3df44
mlsagar <sagar.maharjan617571@outlook.com>
2024-04-10 20:42:19 -0500

    Add: implemented login in the project

--- src/librarysystem/LoginWindow.java
+++ src/librarysystem/LoginWindow.java
@@ -4,8 +4,16 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
 import javax.swing.Box;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -14,37 +22,27 @@ import javax.swing.JPasswordField;
 import javax.swing.JSeparator;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 import javax.swing.JOptionPane;
 
 import business.ControllerInterface;
-
+import business.LoginException;
 import business.SystemController;
+import librarysystem.screens.login;
 
 
 public class LoginWindow extends JFrame implements LibWindow {
+	ControllerInterface ci = new SystemController();
     public static final LoginWindow INSTANCE = new LoginWindow();
 	
 	private boolean isInitialized = false;
 	
-	private JPanel mainPanel;
-	private JPanel upperHalf;
-	private JPanel middleHalf;
-	private JPanel lowerHalf;
-	private JPanel container;
-	
-	private JPanel topPanel;
-	private JPanel middlePanel;
-	private JPanel lowerPanel;
-	private JPanel leftTextPanel;
-	private JPanel rightTextPanel;
-	
-	private JTextField username;
-	private JTextField password;
-	private JLabel label;
-	private JButton loginButton;
-	private JButton logoutButton;
-	
-	
+	private JPanel contentPane;
+	private JTextField usernameField;
+	private JPasswordField passwordField;
+	private JButton btnNewButton;
 	
 	
 	public boolean isInitialized() {
@@ -62,134 +60,96 @@ public class LoginWindow extends JFrame implements LibWindow {
     private LoginWindow () {}
     
     public void init() {     		
-    		mainPanel = new JPanel();
-    		defineUpperHalf();
-    		defineMiddleHalf();
-    		defineLowerHalf();
-    		BorderLayout bl = new BorderLayout();
-    		bl.setVgap(30);
-    		mainPanel.setLayout(bl);
-    					
-    		mainPanel.add(upperHalf, BorderLayout.NORTH);
-    		mainPanel.add(middleHalf, BorderLayout.CENTER);
-    		mainPanel.add(lowerHalf, BorderLayout.SOUTH);
-    		getContentPane().add(mainPanel);
-    		isInitialized(true);
-    		pack();
-    		//setSize(660, 500);
+    	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 753, 548);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 254, 255));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-    	
+		setContentPane(contentPane);
+		
+		
+		 String currDirectory = System.getProperty("user.dir");
+         String pathToImage = Paths.get(currDirectory, "src", "librarysystem", "loginimage.jpg").toString();
+         Image background = Toolkit.getDefaultToolkit().createImage(pathToImage);
+        
+         System.out.println(pathToImage);
+		
+		JLabel layout = new JLabel("New label");
+		layout.setBounds(5, 5, 347, 510);
+		layout.setIcon(new ImageIcon(login.class.getResource("/librarysystem/loginbg.jpg")));
+		
+		JLabel header = new JLabel("Log in to your account");
+		header.setBounds(391, 11, 302, 46);
+		header.setHorizontalAlignment(SwingConstants.LEFT);
+		header.setVerticalAlignment(SwingConstants.TOP);
+		header.setFont(new Font("Tahoma", Font.PLAIN, 27));
+		
+		JLabel username = new JLabel("Username");
+		username.setBounds(391, 87, 99, 17);
+		username.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		
+		usernameField = new JTextField();
+		usernameField.setBounds(391, 135, 319, 59);
+		usernameField.setBackground(new Color(255, 255, 255));
+		usernameField.setHorizontalAlignment(SwingConstants.LEFT);
+		usernameField.setColumns(10);
+		
+		JLabel password = new JLabel("Password");
+		password.setBounds(391, 217, 89, 27);
+		password.setBackground(new Color(255, 146, 0));
+		password.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		
+		btnNewButton = new JButton("Log in");
+		btnNewButton.setBounds(391, 411, 319, 59);
+		btnNewButton.setBorder(new CompoundBorder(new LineBorder(new Color(0, 0, 0), 1, true), null));
+		btnNewButton.setOpaque(true);
+		btnNewButton.setForeground(new Color(0, 0, 0));
+		btnNewButton.setBackground(new Color(170, 121, 65));
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		addLoginButtonListener(btnNewButton);
+		
+		contentPane.setLayout(null);
+		contentPane.add(layout);
+		contentPane.add(header);
+		contentPane.add(username);
+		contentPane.add(usernameField);
+		contentPane.add(password);
+		contentPane.add(btnNewButton);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(391, 284, 319, 59);
+		contentPane.add(passwordField);    	
     }
-    private void defineUpperHalf() {
-    		
-    		upperHalf = new JPanel();
-    		upperHalf.setLayout(new BorderLayout());
-    		defineTopPanel();
-    		defineMiddlePanel();
-    		defineLowerPanel();
-    		upperHalf.add(topPanel, BorderLayout.NORTH);
-    		upperHalf.add(middlePanel, BorderLayout.CENTER);
-    		upperHalf.add(lowerPanel, BorderLayout.SOUTH);
-    		
-    	}
-    	private void defineMiddleHalf() {
-    		middleHalf = new JPanel();
-    		middleHalf.setLayout(new BorderLayout());
-    		JSeparator s = new JSeparator();
-    		s.setOrientation(SwingConstants.HORIZONTAL);
-    		//middleHalf.add(Box.createRigidArea(new Dimension(0,50)));
-    		middleHalf.add(s, BorderLayout.SOUTH);
-    		
-    	}
-    	private void defineLowerHalf() {
-
-    		lowerHalf = new JPanel();
-    		lowerHalf.setLayout(new FlowLayout(FlowLayout.LEFT));
-    		
-    		JButton backButton = new JButton("<= Back to Main");
-    		addBackButtonListener(backButton);
-    		lowerHalf.add(backButton);
-    		
-    	}
-    	private void defineTopPanel() {
-    		topPanel = new JPanel();
-    		JPanel intPanel = new JPanel(new BorderLayout());
-    		intPanel.add(Box.createRigidArea(new Dimension(0,20)), BorderLayout.NORTH);
-    		JLabel loginLabel = new JLabel("Login");
-    		Util.adjustLabelFont(loginLabel, Color.BLUE.darker(), true);
-    		intPanel.add(loginLabel, BorderLayout.CENTER);
-    		topPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
-    		topPanel.add(intPanel);
-    		
-    	}
     	
-    	
-    	
-    	private void defineMiddlePanel() {
-    		middlePanel=new JPanel();
-    		middlePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
-    		defineLeftTextPanel();
-    		defineRightTextPanel();
-    		middlePanel.add(leftTextPanel);
-    		middlePanel.add(rightTextPanel);
-    	}
-    	private void defineLowerPanel() {
-    		lowerPanel = new JPanel();
-    		loginButton = new JButton("Login");
-    		addLoginButtonListener(loginButton);
-    		lowerPanel.add(loginButton);
-    	}
-
-    	private void defineLeftTextPanel() {
-    		
-    		JPanel topText = new JPanel();
-    		JPanel bottomText = new JPanel();
-    		topText.setLayout(new FlowLayout(FlowLayout.LEFT,5,0));
-    		bottomText.setLayout(new FlowLayout(FlowLayout.LEFT,5,0));		
-    		
-    		username = new JTextField(10);
-    		label = new JLabel("Username");
-    		label.setFont(Util.makeSmallFont(label.getFont()));
-    		topText.add(username);
-    		bottomText.add(label);
-    		
-    		leftTextPanel = new JPanel();
-    		leftTextPanel.setLayout(new BorderLayout());
-    		leftTextPanel.add(topText,BorderLayout.NORTH);
-    		leftTextPanel.add(bottomText,BorderLayout.CENTER);
-    	}
-    	private void defineRightTextPanel() {
-    		
-    		JPanel topText = new JPanel();
-    		JPanel bottomText = new JPanel();
-    		topText.setLayout(new FlowLayout(FlowLayout.LEFT,5,0));
-    		bottomText.setLayout(new FlowLayout(FlowLayout.LEFT,5,0));		
-    		
-    		password = new JPasswordField(10);
-    		label = new JLabel("Password");
-    		label.setFont(Util.makeSmallFont(label.getFont()));
-    		topText.add(password);
-    		bottomText.add(label);
-    		
-    		rightTextPanel = new JPanel();
-    		rightTextPanel.setLayout(new BorderLayout());
-    		rightTextPanel.add(topText,BorderLayout.NORTH);
-    		rightTextPanel.add(bottomText,BorderLayout.CENTER);
-    	}
-    	
-    	private void addBackButtonListener(JButton butn) {
-    		butn.addActionListener(evt -> {
-    			LibrarySystem.hideAllWindows();
-    			LibrarySystem.INSTANCE.setVisible(true);
-    		});
-    	}
-    	
-    	private void addLoginButtonListener(JButton butn) {
-    		butn.addActionListener(evt -> {
-    			JOptionPane.showMessageDialog(this,"Successful Login");
-    				
-    		});
-    	}
+	private void addBackButtonListener(JButton butn) {
+		butn.addActionListener(evt -> {
+			LibrarySystem.hideAllWindows();
+			LibrarySystem.INSTANCE.setVisible(true);
+		});
+	}
+	
+	private void addLoginButtonListener(JButton butn) {
+		btnNewButton.addActionListener(evt -> {
+			try {
+				String password = new String(passwordField.getPassword());
+				
+				ci.login(usernameField.getText(), password);
+					Dashboard.INSTANCE.init();
+				Util.centerFrameOnDesktop(Dashboard.INSTANCE);					
+				Dashboard.INSTANCE.setVisible(true);
+				LibrarySystem.hideAllWindows();;
+//					LoginWindow.INSTANCE.setVisible(false);
+				JOptionPane.showMessageDialog(this,"Successful Login");
+			} catch (LoginException e) {
+				// TODO Auto-generated catch block
+//					e.printStackTrace();
+				System.out.println(passwordField.getPassword());
+				JOptionPane.showMessageDialog(this, e.getMessage());
+			}
+				
+		});
+	}
 	
         
     
