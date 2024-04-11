a83fc7ab9a0c5181e7be789515a44d62efc886fe
roshan <45297269+bad-logic@users.noreply.github.com>
2024-04-10 21:46:37 -0500

    add dashboard

--- src/librarysystem/Main.java
+++ src/librarysystem/Main.java
@@ -6,6 +6,8 @@ import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 
+import librarysystem.screens.LoginScreen;
+
 
 
 public class Main {
@@ -13,21 +15,16 @@ public class Main {
 	public static void main(String[] args) {
 	      EventQueue.invokeLater(() -> 
 	         {
-	            LibrarySystem.INSTANCE.setTitle("Sample Library Application");
-	            LibrarySystem.INSTANCE.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	            LoginScreen.INSTANCE.setTitle("Library Application");
+	            LoginScreen.INSTANCE.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	            
-	            LibrarySystem.INSTANCE.init();
-	            centerFrameOnDesktop(LibrarySystem.INSTANCE);
-	            LibrarySystem.INSTANCE.setVisible(true);
+	            LoginScreen.INSTANCE.init();
+	            centerFrameOnDesktop(LoginScreen.INSTANCE);
+	            LoginScreen.INSTANCE.setVisible(true);
 	         });
 	   }
 	   
 	   public static void centerFrameOnDesktop(Component f) {
-			Toolkit toolkit = Toolkit.getDefaultToolkit();
-			int height = toolkit.getScreenSize().height;
-			int width = toolkit.getScreenSize().width;
-			int frameHeight = f.getSize().height;
-			int frameWidth = f.getSize().width;
-			f.setLocation(((width - frameWidth) / 2), (height - frameHeight) / 3);
+		   Util.centerFrameOnDesktop(f);
 		}
 }
