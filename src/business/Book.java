effa1834b080d64d02b1f043f981945c132f5cc8
roshan <45297269+bad-logic@users.noreply.github.com>
2024-04-13 07:38:55 -0500

    fix: resolve checkout bugs

--- src/business/Book.java
+++ src/business/Book.java
@@ -18,8 +18,8 @@ final public class Book implements Serializable {
 	private String isbn;
 	private String title;
 	private int maxCheckoutLength;
+
 	public Book(String isbn, String title, int maxCheckoutLength, List<Author> authors) {
-		
 		this(isbn, title, maxCheckoutLength, 1 ,authors);	
 	}
 	
@@ -51,14 +51,11 @@ final public class Book implements Serializable {
 	}
 	
 	public void addCopy(int count) {
-		System.out.println("From add copy method: " + count);
 		BookCopy[] newArr = new BookCopy[copies.length + count];
 		System.arraycopy(copies, 0, newArr, 0, copies.length);
-		System.out.println("From add copy method array length: " + newArr.length);
 		for(int i=copies.length;i<copies.length + count;i++) {
 			newArr[i] = new BookCopy(this, i + 1, true);
 		}
-		System.out.println(newArr);
 		copies = newArr;
 	}
 	
--- src/business/ControllerInterface.java
+++ src/business/ControllerInterface.java
@@ -10,7 +10,7 @@ public interface ControllerInterface {
 	public void createMember(String firstName, String lastName, String contact,String street, String city,String state, int zipCode);
 	public void createBook(String isbn, String title, int maxCheckoutLength, int copyCount, List<String> authorId);
 	public void createBookCopies(String isbn, int copyCount);
-	public void addCheckoutRecord(String isbn,int memberId);
+	public void addCheckoutRecord(String isbn,int memberId) throws LoginException;
 	public List<Integer> allMemberIds();
 	public List<String> allBookIds();
 	public List<Book> allBooks();
